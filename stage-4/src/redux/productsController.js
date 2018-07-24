23a10bc2eba1269f0df365741848443a509eb19e
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-07-24 15:08:37 -0600

    Stage-4 Complete.

--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const getAllProducts = function() {
+export function getAllProducts() {
     return axios.get("https://practiceapi.devmountain.com/products/")
     .then(response => response.data);
 }
